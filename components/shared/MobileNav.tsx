2ad2f4400d77b98393371f24c09c3f760453ac7e
Abdallah Noor <125575767+abdallanoor@users.noreply.github.com>
2024-03-07 11:42:28 +0200

    Add files via upload

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,5 +1,10 @@
 "use client";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { navLinks } from "@/constants";
 import { SignedOut } from "@clerk/clerk-react";
 import { SignedIn, UserButton } from "@clerk/nextjs";
@@ -7,59 +12,68 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "../ui/button";
+import { Menu, X } from "lucide-react";
 
 export default function MobileNav() {
   const pathname = usePathname();
 
   return (
     <header className="header">
-      <Link href="/" className="flex items-center gap-2 md:py-2">
-        <Image
-          src="/assets/images/logo-text.svg"
-          alt="logo"
-          width={180}
-          height={28}
-        />
-      </Link>
       <nav className="flex gap-2">
         <SignedIn>
-          <UserButton afterSignOutUrl="/" />
           <Sheet>
             <SheetTrigger>
-              <Image
+              {/* <Image
                 src="/assets/icons/menu.svg"
                 alt="menu"
                 width={32}
                 height={32}
                 className="cursor-pointer"
-              />
+              /> */}
+              <Menu className="w-7 h-7" />
             </SheetTrigger>
-            <SheetContent className="sheet-content bg-[#09090b] border-stone-800 sm:w-64">
+            <SheetContent
+              side={"left"}
+              className="sheet-content bg-[#171717] border-stone-800 sm:w-64"
+            >
               <>
-                <Image
-                  src="/assets/images/logo-text.svg"
-                  alt="logo"
-                  width={152}
-                  height={23}
-                />
+                <div className="pl-3 flex justify-between">
+                  <Image
+                    src="/assets/images/logo.svg"
+                    alt="logo"
+                    width={80}
+                    height={23}
+                  />
+                  <SheetClose className="flex h-8 w-8  items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white focus-visible:outline-none">
+                    <X className="h-4 w-4" />
+                    <span className="sr-only">Close</span>
+                  </SheetClose>
+                </div>
+
                 <ul className="header-nav_elements">
                   {navLinks.map((link) => {
                     const isActive = link.route === pathname;
                     return (
                       <li
                         key={link.route}
-                        className={`${
-                          isActive && "gradient-text"
-                        } flex whitespace-nowrap text-dark-500`}
+                        // className={`${
+                        //   isActive && "bg-[#2f2f2f]"
+                        // } flex whitespace-nowrap rounded text-[#ececec] p-2 w-full`}
+                        className={` rounded-lg  w-full  ${
+                          isActive
+                            ? "bg-[#2f2f2f] "
+                            : " hover:bg-[#212121] text-[#ececec]"
+                        }`}
                       >
                         <Link className="sidebar-link" href={link.route}>
                           <Image
                             src={link.icon}
                             alt="logo"
-                            width={24}
-                            height={24}
-                            className={`${isActive && "brightness-200"}`}
+                            width={20}
+                            height={20}
+                            className="brightness-200 "
                           />
+
                           {link.label}
                         </Link>
                       </li>
@@ -77,6 +91,17 @@ export default function MobileNav() {
           </Button>
         </SignedOut>
       </nav>
+      <Link href="/" className="flex items-center gap-2 md:py-2">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="logo"
+          width={80}
+          height={28}
+        />
+      </Link>
+      <SignedIn>
+        <UserButton afterSignOutUrl="/" />
+      </SignedIn>
     </header>
   );
 }
