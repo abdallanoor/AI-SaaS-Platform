800849eb43b40689ee92eedf3d5fe39c175f6276
Abdallah <abdallaahnoor@gmail.com>
2024-03-09 20:24:01 +0200

    add database

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,18 +1,17 @@
 "use client";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import {
   Sheet,
   SheetClose,
   SheetContent,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { Button } from "../ui/button";
 import { navLinks } from "@/constants";
-import { SignedOut } from "@clerk/clerk-react";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { Menu, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Button } from "../ui/button";
-import { Menu, X } from "lucide-react";
 
 export default function MobileNav() {
   const pathname = usePathname();
@@ -23,74 +22,62 @@ export default function MobileNav() {
         <SignedIn>
           <Sheet>
             <SheetTrigger>
-              {/* <Image
-                src="/assets/icons/menu.svg"
-                alt="menu"
-                width={32}
-                height={32}
-                className="cursor-pointer"
-              /> */}
               <Menu className="w-7 h-7" />
             </SheetTrigger>
             <SheetContent
-              side={"left"}
+              side="left"
               className="sheet-content bg-[#171717] border-stone-800 sm:w-64"
             >
-              <>
-                <div className="pl-3 flex justify-between">
-                  <Image
-                    src="/assets/images/logo.svg"
-                    alt="logo"
-                    width={80}
-                    height={23}
-                  />
-                  <SheetClose className="flex h-8 w-8  items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white focus-visible:outline-none">
-                    <X className="h-4 w-4" />
-                    <span className="sr-only">Close</span>
-                  </SheetClose>
-                </div>
+              <div className="pl-3 flex justify-between">
+                <Image
+                  src="/assets/images/logo.svg"
+                  alt="logo"
+                  width={80}
+                  height={23}
+                />
+                <SheetClose className="flex h-10 w-10 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white focus-visible:outline-none">
+                  <X className="h-4 w-4" />
+                  <span className="sr-only">Close</span>
+                </SheetClose>
+              </div>
 
-                <ul className="header-nav_elements">
-                  {navLinks.map((link) => {
-                    const isActive = link.route === pathname;
-                    return (
-                      <li
-                        key={link.route}
-                        // className={`${
-                        //   isActive && "bg-[#2f2f2f]"
-                        // } flex whitespace-nowrap rounded text-[#ececec] p-2 w-full`}
-                        className={` rounded-lg  w-full  ${
-                          isActive
-                            ? "bg-[#2f2f2f] "
-                            : " hover:bg-[#212121] text-[#ececec]"
-                        }`}
-                      >
-                        <Link className="sidebar-link" href={link.route}>
-                          <Image
-                            src={link.icon}
-                            alt="logo"
-                            width={20}
-                            height={20}
-                            className="brightness-200 "
-                          />
-
-                          {link.label}
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </>
+              <ul className="header-nav_elements">
+                {navLinks.map((link) => {
+                  const isActive = link.route === pathname;
+                  return (
+                    <li
+                      key={link.route}
+                      className={`rounded-lg w-full ${
+                        isActive
+                          ? "bg-[#2f2f2f]"
+                          : "hover:bg-[#212121] text-[#ececec]"
+                      }`}
+                    >
+                      <Link className="sidebar-link" href={link.route}>
+                        <Image
+                          src={link.icon}
+                          alt="logo"
+                          width={20}
+                          height={20}
+                          className="brightness-200"
+                        />
+                        {link.label}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
             </SheetContent>
           </Sheet>
         </SignedIn>
 
         <SignedOut>
-          <Button asChild className="button bg-purple-gradient bg-cover">
+          <Button asChild>
             <Link href="/sign-in">Login</Link>
           </Button>
         </SignedOut>
       </nav>
+
       <Link href="/" className="flex items-center gap-2 md:py-2">
         <Image
           src="/assets/images/logo.svg"
@@ -99,6 +86,7 @@ export default function MobileNav() {
           height={28}
         />
       </Link>
+
       <SignedIn>
         <UserButton afterSignOutUrl="/" />
       </SignedIn>
